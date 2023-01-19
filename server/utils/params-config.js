1495153b7bd4e6328cca91566c72b95a26dab31d
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2023-01-19 00:30:29 -0600

    Created upload route to upload image to the S3 bucket

---
+++ server/utils/params-config.js
@@ -0,0 +1,16 @@
+const { v4: uuidv4 } = require('uuid');
+
+const params = (fileName) => {
+  const myFile = fileName.originalname.split('.');
+  const fileType = myFile[myFile.length - 1];
+
+  const imageParams = {
+    // Replace the <My_Bucket_Name> with the name of your own S3 bucket
+    Bucket: 'user-images-7d4d7305-4d93-4495-8b60-1ab96a2a8e22',
+    Key: `${uuidv4()}.${fileType}`,
+    Body: fileName.buffer,
+  };
+  return imageParams;
+};
+
+module.exports = params;
